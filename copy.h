6891c8cb2cb1e2d594a74c42c1ca559c54794983
Azubuike Ibe <X00191169@myTUDublin.ie>
2022-10-27 14:36:54 +0100

    Add project files.

---
+++ copy.h
@@ -0,0 +1,24 @@
+#pragma once
+
+
+class MyCopy
+{
+private:
+
+
+public:
+	// arr is the integer pointer
+	// which stores the address of our vector
+	T* arr;
+
+	// capacity is the total storage
+	// capacity of the vector
+	int capacity;
+
+	// current is the number of elements
+	// currently present in the vector
+	int current;
+
+
+
+};
